16522b32f86db622807dd94bb8e17514662905b1
LeandroRocha <leandro@eulabs.com.br>
2022-04-25 17:11:44 -0300

    move api datails from domain class to data class

---
+++ lib/data/usecases/remote_authentication.dart
@@ -0,0 +1,28 @@
+import 'package:meta/meta.dart';
+
+import '../../domain/usecases/usecases.dart';
+import '../http/http.dart';
+
+class RemoteAuthentication {
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteAuthentication({@required this.httpClient, @required this.url});
+
+  Future<void> auth(AuthenticationParams params) async {
+    final body = RemoteAuthenticationParams.fromDomain(params).toJson();
+    await httpClient.request(url: url, method: 'post', body: body);
+  }
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({@required this.email, @required this.password});
+
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams entity) =>
+      RemoteAuthenticationParams(email: entity.email, password: entity.secret);
+
+  Map toJson() => {'email': email, 'password': password};
+}
--- test/data/http/remote_authentication_test.dart
+++ test/data/http/remote_authentication_test.dart
@@ -1,28 +1,9 @@
 import 'package:faker/faker.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
-import 'package:survey/domain/usecases/authentication.dart';
+import 'package:survey/data/http/http_client.dart';
+import 'package:survey/data/usecases/usecases.dart';
+import 'package:survey/domain/usecases/usecases.dart';
 import 'package:test/test.dart';
-
-class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
-
-  RemoteAuthentication({@required this.httpClient, @required this.url});
-
-  Future<void> auth(AuthenticationParams params) async {
-    final body = {'email': params.email, 'password': params.secret};
-    await httpClient.request(url: url, method: 'post', body: body);
-  }
-}
-
-abstract class HttpClient {
-  Future<void> request({
-    @required String url,
-    @required String method,
-    Map body,
-  });
-}
 
 class HttpClientSpy extends Mock implements HttpClient {}
 

9aba5667b5690b65597df0ef07c1f8ac223c205c
LeandroRocha <leandro@eulabs.com.br>
2022-04-27 00:24:25 -0300

    ensure HttpAdapter calls post correct URL

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,38 @@
+import 'package:faker/faker.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart';
+import 'package:meta/meta.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+class ClienteSpy extends Mock implements Client {}
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<void> request({
+    @required String url,
+    @required String method,
+  }) async {
+    client.post(url);
+  }
+}
+
+void main() {
+  group('post', () {
+    test('Shoudl call post with correct values', () async {
+      // arrange
+      final client = ClienteSpy();
+      final sut = HttpAdapter(client);
+      final url = faker.internet.httpUrl();
+
+      // action
+      await sut.request(url: url, method: 'post');
+
+      //assert
+      verify(client.post(url));
+    });
+  });
+}

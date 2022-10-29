1ab5d7f3e61bdfbb1e2f09190584ecc83b7b8660
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2022-10-29 18:15:21 -0300

    ensure StreamLoginPresenter emits error if email is invalid

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,19 +1,32 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
 
 abstract class Validation {
   String validade({@required String field, @required String value});
 }
 
+class LoginState {
+  String emailError;
+}
+
 class StreamLoginPresenter {
   final Validation validation;
+  final _controller = StreamController<LoginState>.broadcast();
+
+  var _state = LoginState();
+
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
 
   StreamLoginPresenter({@required this.validation});
 
   void validadeEmail(String email) {
-    validation.validade(field: 'email', value: email);
+    _state.emailError = validation.validade(field: 'email', value: email);
+    _controller.add(_state);
   }
 }
 
@@ -34,5 +47,16 @@ void main() {
     sut.validadeEmail(email);
 
     verify(validation.validade(field: 'email', value: email)).called(1);
+  });
+
+  test('Should emit error if validation fails', () {
+    when(validation.validade(
+      field: anyNamed('field'),
+      value: anyNamed('value'),
+    )).thenReturn('error');
+
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validadeEmail(email);
   });
 }

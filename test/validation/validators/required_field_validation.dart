cd3192580dc585cb6b12f7697dac9c1858a0f612
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2022-11-27 09:42:41 -0300

    ensure RequiredFieldValidation returns error if value is null

--- test/validation/validators/required_field_validation.dart
+++ test/validation/validators/required_field_validation.dart
@@ -13,7 +13,7 @@ class RequiredFieldValidation implements FieldValidation {
 
   @override
   String validate(String value) {
-    return value.isEmpty ? 'Campo obrigatório' : null;
+    return value?.isNotEmpty == true ? null : 'Campo obrigatório';
   }
 }
 
@@ -30,5 +30,9 @@ void main() {
 
   test('Should return error if value is empty', () {
     expect(sut.validate(''), 'Campo obrigatório');
+  });
+
+  test('Should return error if value is null', () {
+    expect(sut.validate(null), 'Campo obrigatório');
   });
 }

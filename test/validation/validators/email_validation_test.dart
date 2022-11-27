67fd14a42864b9b63ca162f9fa1c736e5a8d6470
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2022-11-27 10:02:36 -0300

    ensure EmailValidation returns null if email is empty

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,27 @@
+import 'package:survey/validation/protocols/protocols.dart';
+import 'package:test/test.dart';
+
+class EmailValidation implements FieldValidation {
+  final String field;
+
+  EmailValidation(this.field);
+
+  String validate(String value) {
+    return null;
+  }
+}
+
+main() {
+  test('Should return null if email is empty', () {
+    final sut = EmailValidation('any_field');
+    expect(sut.validate(''), null);
+  });
+
+  test('Should return null if email is null', () {
+    final sut = EmailValidation('any_field');
+
+    final error = sut.validate(null);
+
+    expect(error, null);
+  });
+}

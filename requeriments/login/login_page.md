ccaf25862e3bb8f20bf2444447cbce859ae30b47
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2022-12-03 17:34:05 -0300

    ensure LoginPage loads with correct initial state

---
+++ requeriments/login/login_page.md
@@ -0,0 +1,20 @@
+# Login Page
+
+## Regras
+1. ✅ Os campos devem começar sem exibir mensagem de erro
+2. ✅ o botão de fazer login deve comerçar desabilitado
+3. Após digitar algo em um campo, a mensagem de erro só deve sumir se o campo for válido
+4. Validar email quando o usuário digitar no campo
+5. Mostrar mensagem de erro se o email for inválido
+6. Remover mensagem de erro se o emall for válido
+7. Validar senha quadno o usuário digitar no campo
+8. Mostar mensagem de erro se a senha for inválida
+9. Remover mensagem de erro se a senha for válida
+10. Habilitar o botão de fazer login se algum campo for inválido
+11. Desabilitar o botão de fazer login se algum campo for inválido
+12. Exibir mensagem de erro caso o login falhe
+13. Autenticar usuário com email e senha informados no formulario
+14. Exibir mensagem de erro caso o login falhe
+15. Gravar o token do usuário no cache para mantê-lo conectado
+16. Redirecionar o usuário para tela de lista de enquetes
+17. Esconder loading no fim da ação de login
---
+++ test/ui/pages/login_pages_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:survey/ui/pages/pages.dart';
+
+void main() {
+  testWidgets('Should load with', (WidgetTester tester) async {
+    final loginPage = MaterialApp(home: LoginPage());
+    await tester.pumpWidget(loginPage);
+
+    final emailTextChildren = find.descendant(
+        of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
+    expect(
+      emailTextChildren,
+      findsOneWidget,
+      reason:
+          'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the hint text',
+    );
+
+    final passwordTextChildren = find.descendant(
+        of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
+    expect(
+      passwordTextChildren,
+      findsOneWidget,
+      reason:
+          'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the hint text',
+    );
+
+    final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
+    expect(button.onPressed, null);
+  });
+}

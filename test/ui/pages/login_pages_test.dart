33e7c70e6e69d1dbaa80e343346a96f8a1580e96
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2022-12-22 15:17:27 -0300

    move code to helper methods

--- test/ui/pages/login_pages_test.dart
+++ test/ui/pages/login_pages_test.dart
@@ -17,38 +17,46 @@ void main() {
   StreamController<bool> isFormValidController;
   StreamController<bool> isLoadingController;
 
-  Future loadPage(WidgetTester tester) async {
-    presenter = LoginPresenterSpy();
+  void initStreams() {
     emailErrorController = StreamController<String>();
+    passwordErrorController = StreamController<String>();
+    mainErrorController = StreamController<String>();
+    isFormValidController = StreamController<bool>();
+    isLoadingController = StreamController<bool>();
+  }
+
+  void mockStreams() {
     when(presenter.emailErrorStream)
         .thenAnswer((_) => emailErrorController.stream);
-
-    passwordErrorController = StreamController<String>();
     when(presenter.passwordErrorStream)
         .thenAnswer((_) => passwordErrorController.stream);
-
-    mainErrorController = StreamController<String>();
     when(presenter.mainErrorStream)
         .thenAnswer((_) => mainErrorController.stream);
-
-    isFormValidController = StreamController<bool>();
     when(presenter.isFormValidStream)
         .thenAnswer((_) => isFormValidController.stream);
-
-    isLoadingController = StreamController<bool>();
     when(presenter.isLoadingStream)
         .thenAnswer((_) => isLoadingController.stream);
+  }
+
+  void closeStreams() {
+    emailErrorController.close();
+    passwordErrorController.close();
+    mainErrorController.close();
+    isFormValidController.close();
+    isLoadingController.close();
+  }
+
+  Future loadPage(WidgetTester tester) async {
+    presenter = LoginPresenterSpy();
+    initStreams();
+    mockStreams();
 
     final loginPage = MaterialApp(home: LoginPage(presenter));
     await tester.pumpWidget(loginPage);
   }
 
   tearDown(() {
-    emailErrorController.close();
-    passwordErrorController.close();
-    mainErrorController.close();
-    isFormValidController.close();
-    isLoadingController.close();
+    closeStreams();
   });
 
   testWidgets('Should load with', (WidgetTester tester) async {

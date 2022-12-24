51922adf1fa72045199a242bc646e4689f34f336
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2022-12-24 19:22:18 -0300

    move HttpAdapter to its own file

--- lib/main/factories/http/http_client_factory.dart
+++ lib/main/factories/http/http_client_factory.dart
@@ -1,20 +1,9 @@
 import 'package:http/http.dart';
 
 import '../../../data/http/http.dart';
+import '../../../infra/http/http.dart';
 
 HttpClient makeHttpAdapter() {
   final client = Client();
   return HttpAdapter(client);
-}
-
-class HttpAdapter implements HttpClient {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  @override
-  Future<Map> request({String url, String method, Map body}) {
-    // TODO: implement request
-    throw UnimplementedError();
-  }
 }
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,41 +1,10 @@
-import 'dart:convert';
-
 import 'package:faker/faker.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
-import 'package:survey/data/http/http.dart';
+import 'package:survey/infra/http/http.dart';
 import 'package:test/test.dart';
 
 class ClienteSpy extends Mock implements Client {}
-
-class HttpAdapter implements HttpClient {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  Future<Map> request({
-    @required String url,
-    @required String method,
-    Map body,
-  }) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json',
-    };
-
-    final jsonBody = body != null ? jsonEncode(body) : null;
-    final response = await client.post(url, headers: headers, body: jsonBody);
-    if (response?.statusCode == 200) {
-      return response?.body?.isNotEmpty == true
-          ? jsonDecode(response.body)
-          : null;
-    } else {
-      return null;
-    }
-  }
-}
 
 void main() {
   HttpAdapter sut;

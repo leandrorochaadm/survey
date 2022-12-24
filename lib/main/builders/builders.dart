a6624ec47577565c448f08b5ebb238aa358ecb7b
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2022-12-24 18:37:34 -0300

    add ValidationsBuilder

---
+++ lib/main/builders/builders.dart
@@ -0,0 +1,1 @@
+export 'validations_builder.dart';
--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,3 +1,5 @@
+import 'package:survey/main/builders/builders.dart';
+
 import '../../../../presentation/protocols/validation.dart';
 import '../../../../validation/protocols/protocols.dart';
 import '../../../../validation/validators/validators.dart';
@@ -8,8 +10,7 @@ Validation makeLoginValidation() {
 
 List<FieldValidation> makeLoginValidations() {
   return [
-    RequiredFieldValidation('email'),
-    EmailValidation('email'),
-    RequiredFieldValidation('password'),
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().build(),
   ];
 }

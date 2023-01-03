0e1d19600cc0e783d36da06acca442d1def6664b
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-01-03 14:16:43 -0300

    move SplashPage to its own file

---
+++ lib/ui/pages/splash/splash_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:meta/meta.dart';
+
+import 'splash_presenter.dart';
+
+class SplashPage extends StatelessWidget {
+  final SplashPresenter presenter;
+
+  const SplashPage({@required this.presenter});
+
+  @override
+  Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
+    return Scaffold(
+      appBar: AppBar(title: Text('4Dev')),
+      body: Builder(builder: (context) {
+        presenter.navigateToStream.listen((page) {
+          if (page?.isNotEmpty == true) {
+            Get.offAllNamed(page);
+          }
+        });
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      }),
+    );
+  }
+}
